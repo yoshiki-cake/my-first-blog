66bbc45762dd97759591c541d414b72cf29e5b1e
yoshiki-cake <fukao@vision.ist.i.kyoto-u.ac.jp>
2020-12-10 13:44:56 +0900

    updated to diplay views

--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,15 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    # post = get_object_or_404(Post, pk="")
+    # post.views += 1
+    # post.save()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.views += 1    # ←ここ
+    post.save()            # ←ここ
     return render(request, 'blog/post_detail.html', {'post': post})
 
 @login_required
